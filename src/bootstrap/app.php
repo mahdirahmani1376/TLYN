3811ebcc055f5dbe0b2f7e839a35fd0dbfbea848
mahdi <rahmanimahdi16@gmail.com>
2025-05-13 14:49:32 +0330

    add schedule

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -1,16 +1,21 @@
 <?php
 
+use App\Actions\Order\OrderMatchAction;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->job(OrderMatchAction::class)->everySecond();
+    })
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
